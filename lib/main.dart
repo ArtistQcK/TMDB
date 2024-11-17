484496f504bec750686c3f7c550b3daf132ef30c
Kartel Andrey <ArtistQcK@gmail.com>
2024-11-17 10:38:29 +0200

    update main.dart to use 'const' for app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:dart_lesson/ui/widgets/app/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // comment to commit ss
-  final app = MyApp();
+  const app = MyApp();
   runApp(app);
 }
