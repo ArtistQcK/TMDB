f4f18b59b4cf34eae0f5f2558178a8eacaee8f05
Kartel Andrey <ArtistQcK@gmail.com>
2024-11-17 10:31:57 +0200

    update comment in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:dart_lesson/ui/widgets/app/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // comment to commit
+  // comment to commit ss
   const app = MyApp();
   runApp(app);
 }
